2188dc2cab9a3792e7ba7300cb62b3e972ff4299
Jitender <58380090+jeetu9896@users.noreply.github.com>
2022-06-07 15:45:39 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { cloneElement } from 'react'
 
 function App() {
+
+  const [count, setcount] = React.useState(0);
+  const valueadd = () => {
+
+    setcount(count + 1);
+  }
+  const valueremove  =()=>{
+    if(count<=0){
+      setcount(0)
+    }
+    else{
+
+      setcount(count - 1); 
+    }
+  }
+  const valuedouble=()=>{
+    setcount(count * 2);
+  }
+  //  console.log(count)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      {count%2==0?<h2 style={{color:"green"}}>EVEN</h2>:<h2 style={{color:"red"}}>ODD</h2>}
+      <button onClick={valueadd} style={{margin:"5px"}}>Add</button>
+      <button onClick={valueremove} style={{margin:"5px"}}>Reduce</button>
+      <button onClick={valuedouble} style={{margin:"5px"}}>Dobule</button>
+
+
+
     </div>
   );
 }
